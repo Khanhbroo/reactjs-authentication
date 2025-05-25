7a28549112f81b78d338fa1736d39d1aa34fc6fa
Khanhbroo <khanhmathias532211@gmail.com>
2025-05-25 17:29:32 +0700

    Managin Token Expiration

--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -1,14 +1,15 @@
 import { redirect } from "react-router-dom";
 
-export function getTokenDuration() {
+export const getTokenDuration = () => {
   const storedExpirationDate = localStorage.getItem("expiration");
   const expirationDate = new Date(storedExpirationDate);
   const now = new Date();
   const duration = expirationDate.getTime() - now.getTime();
-  return duration;
-}
 
-export function getAuthToken() {
+  return duration;
+};
+
+export const getAuthToken = () => {
   const token = localStorage.getItem("token");
 
   if (!token) {
@@ -22,17 +23,17 @@ export function getAuthToken() {
   }
 
   return token;
-}
+};
 
-export function tokenLoader() {
+export const tokenLoader = () => {
   const token = getAuthToken();
   return token;
-}
+};
 
-export function checkAuthLoader() {
+export const checkAuthLoader = () => {
   const token = getAuthToken();
 
   if (!token) {
     return redirect("/auth");
   }
-}
+};
