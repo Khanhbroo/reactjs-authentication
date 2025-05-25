86eee9a285389b67ec8e313fbf69e9793c066775
Khanhbroo <khanhmathias532211@gmail.com>
2025-05-25 15:02:59 +0700

    Implementing the Auth Action

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,3 +1,4 @@
+import { redirect } from "react-router-dom";
 import AuthForm from "../components/AuthForm";
 
 function AuthenticationPage() {
@@ -5,3 +6,39 @@ function AuthenticationPage() {
 }
 
 export default AuthenticationPage;
+
+export const action = async ({ request }) => {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get("mode") || "login";
+
+  if (mode !== "login" && mode !== "signup") {
+    throw Response.json({ message: "Unsupport mode" }, { status: 422 });
+  }
+
+  const data = await request.formData();
+  const authData = Object.fromEntries(data.entries());
+
+  console.log(authData);
+
+  const response = await fetch("http://localhost:8080/" + mode, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(authData),
+  });
+
+  if (response.status === 422 || response.status === 401) {
+    return response;
+  }
+
+  if (!response.ok) {
+    throw Response.json(
+      { message: "Could not authenticate user." },
+      { status: 500 }
+    );
+  }
+
+  // Soon: manage that token
+  return redirect("/");
+};
